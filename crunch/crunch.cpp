e647680ef9c6cfd2443f723e7acbf176c5eaa671
Rich Geldreich <richgel99@gmail.com>
2016-06-15 23:21:39 -0700

    Updating github URL

--- crunch/crunch.cpp
+++ crunch/crunch.cpp
@@ -1253,7 +1253,7 @@ static bool check_for_option(int argc, char *argv[], const char *pOption)
 
 static void print_title()
 {
-   console::printf("crunch: Advanced DXTn Texture Compressor - https://github.com/richgel999/crunch");
+   console::printf("crunch: Advanced DXTn Texture Compressor - https://github.com/BinomialLLC/crunch");
    console::printf("Copyright (c) 2010-2016 Binomial LLC");
    console::printf("crnlib version v%u.%02u %s Built %s, %s", CRNLIB_VERSION / 100U, CRNLIB_VERSION % 100U, crnlib_is_x64() ? "x64" : "x86", __DATE__, __TIME__);
    console::printf("");
