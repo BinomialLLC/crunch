e6e5f0de2b8d59a820e25d39dcf643317b8021c6
richgel999 <richgel99@gmail.com>
2019-06-15 00:52:56 -0700

    Fixing copyright at end of file

--- inc/crnlib.h
+++ inc/crnlib.h
@@ -616,27 +616,30 @@ bool crn_decompress_block(const void *pSrc_block, crn_uint32 *pDst_pixels, crn_f
 
 //------------------------------------------------------------------------------
 //
-// crnlib uses the ZLIB license:
-// http://opensource.org/licenses/Zlib
-//
+// crunch/crnlib uses a modified ZLIB license. Specifically, it's the same as zlib except that 
+// public credits for using the library are *required*.
+// 
 // Copyright (c) 2010-2016 Richard Geldreich, Jr. All rights reserved.
 //
 // This software is provided 'as-is', without any express or implied
 // warranty.  In no event will the authors be held liable for any damages
 // arising from the use of this software.
-//
+// 
 // Permission is granted to anyone to use this software for any purpose,
 // including commercial applications, and to alter it and redistribute it
 // freely, subject to the following restrictions:
-//
+// 
 // 1. The origin of this software must not be misrepresented; you must not
-// claim that you wrote the original software. If you use this software
-// in a product, an acknowledgment in the product documentation would be
-// appreciated but is not required.
-//
-// 2. Altered source versions must be plainly marked as such, and must not be
+// claim that you wrote the original software. 
+// 
+// 2. If you use this software in a product, this acknowledgment in the product 
+// documentation or credits is required:
+// 
+// "Crunch Library Copyright (c) 2010-2016 Richard Geldreich, Jr."
+// 
+// 3. Altered source versions must be plainly marked as such, and must not be
 // misrepresented as being the original software.
-//
-// 3. This notice may not be removed or altered from any source distribution.
+// 
+// 4. This notice may not be removed or altered from any source distribution.
 //
 //------------------------------------------------------------------------------
